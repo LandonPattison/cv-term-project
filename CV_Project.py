d1b3f7777b28930c3087d1d4e9e6709b4443e3b7
Mariana Lucio <marianaclucio@outlook.com>
2023-05-03 19:56:06 -0500

    4B complete

--- CV_Project.py
+++ CV_Project.py
@@ -52,25 +52,27 @@ def preprocess_data(input_directory, save_directory, sift_directory, hist_direct
                     # 2. Extract SIFT features on ALL images and save the data.
                     key_points, descriptor = sift.detectAndCompute(resized, None)
                     if descriptor is not None and descriptor.shape[0] >= min_keypoints:  # Check if the descriptor array is not empty and has enough keypoints
-                        sift_path = os.path.join(sift_subdir, f"{img_name.split('.')[0]}_{size[0]}x{size[1]}.npy")
-                        np.save(sift_path, descriptor)
+                       sift_path = os.path.join(sift_subdir, f"{img_name.split('.')[0]}_{size[0]}x{size[1]}.npy")
+                       np.save(sift_path, descriptor)
+
 
 # Pre-process Train data
 train_directory = 'ProjData/Train'
 train_save_directory = 'ProjData/TrainResized'
 train_sift_directory = 'ProjData/TrainSift'
 train_hist_directory = 'ProjData/TrainHist'
-#preprocess_data(train_directory, train_save_directory, train_sift_directory, train_hist_directory)
+preprocess_data(train_directory, train_save_directory, train_sift_directory, train_hist_directory)
 
 # Pre-process Test data
 test_directory = 'ProjData/Test'
 test_save_directory = 'ProjData/TestResized'
 test_sift_directory = 'ProjData/TestSift'
 test_hist_directory = 'ProjData/TestHist'
-#preprocess_data(test_directory, test_save_directory, test_sift_directory, test_hist_directory)
+preprocess_data(test_directory, test_save_directory, test_sift_directory, test_hist_directory)
 
 
-# 4A 50*50 KNN Calssifier
+
+# 4A 50*50 KNN Classifier
 train_features = [] 
 train_labels = [] 
  
@@ -105,11 +107,10 @@ knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(train_features, train_labels)
 
 test_predictions = knn.predict(test_features) 
-
+print("Classification report for 4A:\n")
 print(classification_report(test_labels, test_predictions)) 
 
 
-"""
 # 4C. KNN Classifier
 def load_hist_data(hist_data_directory):
     data = []
@@ -136,6 +137,7 @@ knn.fit(train_hist_data, train_labels)
 
 
 predicted_labels = knn.predict(test_hist_data)
+print("Classification report for 4C:\n")
 print(classification_report(test_labels, predicted_labels))
 
 
@@ -173,10 +175,25 @@ clf.fit(X_train_stacked, y_train_stacked)
 
 # Predict on test data
 y_pred = clf.predict(X_test_stacked)
-
+print("Classification report for 4D:\n")
 # Evaluate the performance of the classifier
 print(classification_report(y_test_stacked, y_pred))
 
+
+#4B SIFT features KNN
+A_train, B_train = load_sift_data(train_sift_directory)
+A_test, B_test = load_sift_data(test_sift_directory)
+
+A_train_stacked, B_train_stacked = stack_descriptors_and_labels(X_train, y_train)
+A_test_stacked, B_test_stacked = stack_descriptors_and_labels(X_test, y_test)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(A_train_stacked, B_train_stacked)
+
+y_pred = knn.predict(A_test_stacked)
+report = classification_report(B_test_stacked, y_pred)
+print("Classification report for 4B:\n")
+print(report)
 
 # Remode the directories
 shutil.rmtree('ProjData/TrainResized')
@@ -185,4 +202,3 @@ shutil.rmtree('ProjData/TrainHist')
 shutil.rmtree('ProjData/TestResized')
 shutil.rmtree('ProjData/TestSift')
 shutil.rmtree('ProjData/TestHist')
-"""
